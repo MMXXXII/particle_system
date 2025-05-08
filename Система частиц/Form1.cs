d45bf2948255e60c9fe124a40b740fb21268fcfd
MMXXII <perfilev621@gmail.com>
2025-05-08 20:53:42 +0800

    Система частиц, часть 1

--- Система частиц/Form1.cs
+++ Система частиц/Form1.cs
@@ -1,93 +1,62 @@
+using static Система_частиц.Emitter;
 using static Система_частиц.Particle;
 
 namespace Система_частиц
 {
+
     public partial class Form1 : Form
     {
-        List<Particle> particles = new List<Particle>();
+        Emitter emitter; // тут убрали явное создание
         public Form1()
         {
             InitializeComponent();
             picDisplay.Image = new Bitmap(picDisplay.Width, picDisplay.Height);
-        }
 
-        private void UpdateState()
-        {
-            foreach (var particle in particles)
+            // а тут теперь вручную создаем
+            emitter = new TopEmitter
             {
-                particle.Life -= 1;  // не трогаем
-                if (particle.Life < 0)
-                {
-                    // тоже не трогаем
-                    particle.Life = 20 + Particle.rand.Next(100);
-                    particle.X = MousePositionX;
-                    particle.Y = MousePositionY;
+                Width = picDisplay.Width,
+                GravitationY = 0.25f
+            };
 
-                    /* это убираем
-                    particle.Direction = Particle.rand.Next(360);
-                    particle.Speed = 1 + Particle.rand.Next(10);
-                    */
-
-                    /* ЭТО ДОБАВЛЯЮ, тут сброс состояния частицы */
-                    var direction = (double)Particle.rand.Next(360);
-                    var speed = 1 + Particle.rand.Next(10);
-
-                    particle.SpeedX = (float)(Math.Cos(direction / 180 * Math.PI) * speed);
-                    particle.SpeedY = -(float)(Math.Sin(direction / 180 * Math.PI) * speed);
-                    /* конец ЭТО ДОБАВЛЯЮ  */
-
-                    // это не трогаем
-                    particle.Radius = 2 + Particle.rand.Next(10);
-                }
-                else
-                {
-                    /* это все убираем, тут у нас старый пересчет положения частицы в пространстве 
-                    var directionInRadians = particle.Direction / 180 * Math.PI;
-                    particle.X += (float)(particle.Speed * Math.Cos(directionInRadians));
-                    particle.Y -= (float)(particle.Speed * Math.Sin(directionInRadians));
-                    */
-
-                    // и добавляем новый, собственно он даже проще становится, 
-                    // так как теперь мы храним вектор скорости в явном виде и его не надо пересчитывать
-                    particle.X += particle.SpeedX;
-                    particle.Y += particle.SpeedY;
-                }
-            }
-        }
-
-
-        // функция рендеринга
-        private void Render(Graphics g)
-        {
-            // утащили сюда отрисовку частиц
-            foreach (var particle in particles)
+            emitter.impactPoints.Add(new GravityPoint
             {
-                particle.Draw(g);
-            }
+                X = (float)(picDisplay.Width * 0.25),
+                Y = picDisplay.Height / 2
+            });
+
+            emitter.impactPoints.Add(new AntiGravityPoint
+            {
+                X = picDisplay.Width / 2,
+                Y = picDisplay.Height / 2
+            });
+
+            emitter.impactPoints.Add(new GravityPoint
+            {
+                X = (float)(picDisplay.Width * 0.75),
+                Y = picDisplay.Height / 2
+            });
+            
         }
 
         // ну и обработка тика таймера, тут просто декомпозицию выполнили
         private void timer1_Tick(object sender, EventArgs e)
         {
-            UpdateState();
+            emitter.UpdateState(); // тут теперь обновляем эмиттер
 
             using (var g = Graphics.FromImage(picDisplay.Image))
             {
-                g.Clear(Color.Black); // А ЕЩЕ ЧЕРНЫЙ ФОН СДЕЛАЮ
-                Render(g);
+                g.Clear(Color.Black);
+                emitter.Render(g); // а тут теперь рендерим через эмиттер
             }
 
             picDisplay.Invalidate();
         }
-
-        private int MousePositionX = 0;
-        private int MousePositionY = 0;
-
         private void picDisplay_MouseMove(object sender, MouseEventArgs e)
         {
-            // в обработчике заносим положение мыши в переменные для хранения положения мыши
-            MousePositionX = e.X;
-            MousePositionY = e.Y;
+            // а тут в эмиттер передаем положение мыфки
+            emitter.MousePositionX = e.X;
+            emitter.MousePositionY = e.Y;
         }
     }
 }
